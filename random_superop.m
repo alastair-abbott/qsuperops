e26b0a2e301ce1eff65af840405827958fe012cf
Alastair Abbott <al.a.abbott@gmail.com>
2022-09-27 16:39:36 +0200

    Start sampling from a projector

--- random_superop.m
+++ random_superop.m
@@ -1,22 +1,33 @@
-function Wr = random_superop(dims,parties,R)
+function Wr = random_superop(dims,parties,R,sampling_method)
 %random_superop Produces a random superoperator in the specified class
 %   W = random_superop(dims,parties,superop_class,R)
 %   dims, parties: specifies the desired structure of the superoperator
 %   R: the number of elements of the superinstrument (default: 1)
+%   sampling_method: the method used to sample random processes (default: 'pure_projection')
 %
-%   The sampling simply generates a random PSD matrix with the correct norm
-%   and projects onto the space of valid superoperators. No claims are made about the uniformity
-%   of this approach (it is certainly not uniform)
+%   Several sampling methods are possible:
+%   'pure_projection': a random projector is generated and scaled to have the correct norm. This is then
+%                      projected on the space of valid superoperators and then mixed with white noise
+%                      so that the resulting operator is PSD. This is the default option.
+%   'PSD_projection': as for 'pure_projection', except initially a random PSD matrix is generated.
+%   'closest_valid': A random projector is generated and then an SDP is used to find the closest
+%                    valid superoperator wrt XXX nrom
+%   Note: no claims about the uniformity of these methods are made (none of them are uniform)
 %   For superinstruments, we essentially measure an extra R-dimensional space in F and take the resulting
 %   post-selected superoperators as the elements of the superinstrument
 %
 % Requires QETLAB for RandomDensityMatrix
 
-% Written by Alastair Abbott 2022, last modified 20 September 2022
+% Written by Alastair Abbott 2022, last modified 27 September 2022
 
     if ~exist('R','var')
         R = 1;
     end
+
+    if ~exist('sampling_method','var')
+        sampling_method = 'pure_projection';
+    end
+
 
     N = length(parties) - 2;
 
@@ -33,8 +44,15 @@ function Wr = random_superop(dims,parties,R)
     end
     d_I = d/d_O;
 
-    % Generate a random PSD matrix
-    W = d_O*RandomDensityMatrix(d);
+    % Generate a random operator
+    switch lower(sampling_method)
+        case 'pure_projection'
+            W = d_O*pure_to_mixed(RandomStateVector(d));
+        case 'psd_projection'
+            W = d_O*RandomDensityMatrix(d);
+        otherwise
+            error('Error: unknown sampling method specified');
+    end
 
     % Project onto valid subspace
     Wr = project_onto_valid_superops(W,dims_extended,parties_extended);
